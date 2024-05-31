cfc84d900cc6ac39587aa11d04c291208a773df9
Gabolobolo <136251040+Gabolobolo@users.noreply.github.com>
2024-05-31 15:33:08 -0500

    Productos.c

--- Productos.c
+++ Productos.c
@@ -1,13 +1,3 @@
-//Se desea desarrollar un sistema de inventarios, el cual implemente las siguientes funcionalidades:
-
-//Ingresar, editar y eliminar productos.
-//Listar productos ingresados.
-//Los productos ingresados deberán tener mínimo las siguientes variables:
-
-//Nombre del producto-Cantidad-Precio
-
-
-
 //Funciones y variables definidas en inventario.h//
 #include "inventario.h"
 
